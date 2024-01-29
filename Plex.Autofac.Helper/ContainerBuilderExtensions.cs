53711d220114e10b08eeddd791b303a242d8d4d2
Tom Lee <nam.brucelee15987@gmail.com>
2024-01-29 14:54:30 +0700

    Add Plex.Autofac.Helper

---
+++ Plex.Autofac.Helper/ContainerBuilderExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Autofac;
+
+namespace Plex.Autofac.Helper;
+
+public static class ContainerBuilderExtensions
+{
+    public static ContainerBuilder RegisterAssemblyWithMetaData<TMetaData, TIService>(this ContainerBuilder containerBuilder) where TIService : class
+    {
+        var metaDatas = typeof(TMetaData).Assembly.GetTypes().Where(t => typeof(TIService).IsAssignableFrom(t) && !t.IsInterface).ToList();
+        foreach (var item in metaDatas)
+        {
+            object? name = (from t in item.CustomAttributes.Where(t => t.ConstructorArguments != null
+                                                               && t.ConstructorArguments.First().Value != null)
+                            select t.ConstructorArguments.First().Value).FirstOrDefault();
+            if (name == null) continue;
+            containerBuilder.RegisterType(item).As<TIService>().WithMetadata("PortalPartMetadata", name);
+        }
+
+        return containerBuilder;
+    }
+}
