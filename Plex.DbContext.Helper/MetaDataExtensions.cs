a93f50ba078b8a83d1e1d0957a054c4ff2543cfe
Tom Lee <nam.brucelee15987@gmail.com>
2024-01-30 13:46:12 +0700

    Add MetaDataExtensions

---
+++ Plex.DbContext.Helper/MetaDataExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Autofac.Features.Metadata;
+
+namespace Plex.DbContext.Helper;
+
+public static class MetaDataExtensions
+{
+    public static Meta<T>? ResolvedMeta<T, TMeta>(this IEnumerable<Meta<T>> interfaces, object metaName)
+    {
+        return (from t in interfaces
+                let metadata = t.Metadata[nameof(TMeta)]
+                where metadata != null && metadata.Equals(metaName)
+                select t).FirstOrDefault();
+    }
+}
