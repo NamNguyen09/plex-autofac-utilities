b43ea8c6f52c1143619649d5a501d563447a5ef0
NamNguyen09 <nam.nguyenvan@orientsoftware.com>
2024-01-31 01:29:29 +0700

    Improve RegisterAssemblyWithMetaData method

--- Plex.Autofac.Helper/ContainerBuilderExtensions.cs
+++ Plex.Autofac.Helper/ContainerBuilderExtensions.cs
@@ -4,18 +4,27 @@ namespace Plex.Autofac.Helper;
 
 public static class ContainerBuilderExtensions
 {
-    public static ContainerBuilder RegisterAssemblyWithMetaData<TMetaData, TIService>(this ContainerBuilder containerBuilder, string metaName)
-                                                                where TMetaData : class
-                                                                where TIService : class
+    public static ContainerBuilder RegisterAssemblyWithMetaData<IService>(this ContainerBuilder containerBuilder, 
+                                                                Type[] assemblyTypes,
+                                                                string metaName)
+                                                                where IService : class
     {
-        var metaDatas = typeof(TMetaData).Assembly.GetTypes().Where(t => typeof(TIService).IsAssignableFrom(t) && !t.IsInterface).ToList();
-        foreach (var item in metaDatas)
+         var types = assemblyTypes.Where(t => typeof(IService)
+                          .IsAssignableFrom(t) && !t.IsInterface).ToList();
+        foreach (Type? item in types)
         {
-            object? name = (from t in item.CustomAttributes.Where(t => t.ConstructorArguments != null
-                                                               && t.ConstructorArguments.First().Value != null)
-                            select t.ConstructorArguments.First().Value).FirstOrDefault();
-            if (name == null) continue;
-            containerBuilder.RegisterType(item).As<TIService>().WithMetadata(metaName, name);
+            if (item == null) continue;
+            object? name = (from t in item.CustomAttributes
+                            where t.ConstructorArguments != null 
+                                  && t.ConstructorArguments.First().Value != null
+                            select t.ConstructorArguments.First().Value)
+                            .FirstOrDefault();
+
+            string? functionName = name?.ToString()?.ToLower() ?? "";
+            if (string.IsNullOrWhiteSpace(functionName)) continue;
+            containerBuilder.RegisterType(item).As<IService>()
+                            .WithMetadata(metaName, functionName)
+                            .InstancePerLifetimeScope();
         }
 
         return containerBuilder;
--- Plex.Autofac.Helper/MetaDataExtensions.cs
+++ Plex.Autofac.Helper/MetaDataExtensions.cs
@@ -4,11 +4,13 @@ namespace Plex.Autofac.Helper;
 
 public static class MetaDataExtensions
 {
-    public static Meta<T>? ResolvedMeta<T>(this IEnumerable<Meta<T>> interfaces, string metaName, string servieName)
+    public static Meta<T>? ResolvedService<T>(this IEnumerable<Meta<T>> interfaces, 
+                                              string metaName, 
+                                              string servieName)
     {
         return (from t in interfaces
                 let metadata = t.Metadata[metaName]
-                where metadata != null && metadata.ToString() == servieName
+                where metadata != null && metadata.ToString()?.ToLower() == servieName.ToLower()
                 select t).FirstOrDefault();
     }
 }
