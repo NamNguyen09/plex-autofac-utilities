1d90f958f1b82d8c33e4c40997110e94d3966444
Tom Lee <nam.brucelee15987@gmail.com>
2024-01-30 14:34:40 +0700

    Fix cannot get resolved service

--- Plex.Autofac.Helper/MetaDataExtensions.cs
+++ Plex.Autofac.Helper/MetaDataExtensions.cs
@@ -4,11 +4,11 @@ namespace Plex.Autofac.Helper;
 
 public static class MetaDataExtensions
 {
-    public static Meta<T>? ResolvedMeta<T, TMeta>(this IEnumerable<Meta<T>> interfaces, object metaName)
+    public static Meta<T>? ResolvedMeta<T>(this IEnumerable<Meta<T>> interfaces, string metaName, string servieName)
     {
         return (from t in interfaces
-                let metadata = t.Metadata[nameof(TMeta)]
-                where metadata != null && metadata.Equals(metaName)
+                let metadata = t.Metadata[metaName]
+                where metadata != null && metadata.ToString() == servieName
                 select t).FirstOrDefault();
     }
 }
