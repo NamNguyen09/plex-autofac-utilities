76a12ce66c3ef897a31165c3ef1414405c9dbc1a
Tom Lee <nam.brucelee15987@gmail.com>
2024-01-31 13:19:33 +0700

    Fix registering service function

--- Plex.Autofac.Helper/ContainerBuilderExtensions.cs
+++ Plex.Autofac.Helper/ContainerBuilderExtensions.cs
@@ -4,18 +4,19 @@ namespace Plex.Autofac.Helper;
 
 public static class ContainerBuilderExtensions
 {
-    public static ContainerBuilder RegisterAssemblyWithMetaData<IService>(this ContainerBuilder containerBuilder, 
+    public static ContainerBuilder RegisterAssemblyWithMetaData<IService>(this ContainerBuilder containerBuilder,
                                                                 Type[] assemblyTypes,
                                                                 string metaName)
                                                                 where IService : class
     {
-         var types = assemblyTypes.Where(t => typeof(IService)
-                          .IsAssignableFrom(t) && !t.IsInterface).ToList();
+        var types = assemblyTypes.Where(t => typeof(IService)
+                         .IsAssignableFrom(t) && !t.IsInterface).ToList();
         foreach (Type? item in types)
         {
             if (item == null) continue;
             object? name = (from t in item.CustomAttributes
-                            where t.ConstructorArguments != null 
+                            where t.AttributeType.Name == metaName
+                                  && t.ConstructorArguments != null
                                   && t.ConstructorArguments.First().Value != null
                             select t.ConstructorArguments.First().Value)
                             .FirstOrDefault();
