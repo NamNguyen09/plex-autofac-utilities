8415733cc841c706a750060485e40b60eae88388
NamNguyen09 <nam.nguyenvan@orientsoftware.com>
2024-06-26 01:27:37 +0700

    Uninstall Azure.Identity and Azure.Extensions.AspNetCore.Configuration.Secrets

--- Plex.Extensions.DbContext/ConfigurationExtensions.cs
+++ Plex.Extensions.DbContext/ConfigurationExtensions.cs
@@ -1,10 +1,6 @@
 ﻿using System.Collections.Concurrent;
 using System.Data.Common;
-using System.Reflection;
-using System.Text.Json;
 using System.Text.RegularExpressions;
-using Azure.Identity;
-using Microsoft.Extensions.Configuration.Json;
 using Microsoft.Extensions.Primitives;
 using static Plex.Extensions.DbContext.Constants;
 
@@ -90,83 +86,6 @@ public static class ConfigurationExtensions
 		value = configuration.GetValue<string>(key) ?? defaultValue;
 		_keyValuePairs.TryAdd(key, value);
 		return value;
-	}
-	public static async Task<WebApplicationBuilder> AddJsonConfigurationFilesAsync(this WebApplicationBuilder builder)
-	{
-		ArgumentNullException.ThrowIfNull(builder);
-
-		if (string.IsNullOrEmpty(builder.Environment.EnvironmentName))
-			builder.Environment.EnvironmentName = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "";
-		string environmentName = builder.Environment.EnvironmentName;
-		var jsonBuilder = builder.Configuration.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
-		if (!string.IsNullOrWhiteSpace(environmentName) && File.Exists("appsettings." + environmentName + ".json"))
-		{
-			jsonBuilder = jsonBuilder.AddJsonFile("appsettings." + environmentName + ".json", optional: true, reloadOnChange: true);
-		}
-
-		bool isLocal = Environment.GetEnvironmentVariable("IsLocal") == "true";
-		if (!isLocal)
-		{
-			string? DAPR_SECRET_STORE = Environment.GetEnvironmentVariable("DAPR_SECRET_STORE");//"localsecretstore";
-			string? keyVaultName = Environment.GetEnvironmentVariable("KV_NAME");
-			if (string.IsNullOrWhiteSpace(DAPR_SECRET_STORE) && !string.IsNullOrWhiteSpace(keyVaultName))
-			{
-				builder.Configuration.AddAzureKeyVault(
-				  new Uri($"https://{keyVaultName}.vault.azure.net/"),
-				  new DefaultAzureCredential());
-			}
-			else if (!string.IsNullOrWhiteSpace(DAPR_SECRET_STORE))
-			{
-				var httpClient = new HttpClient();
-				httpClient.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
-				var baseURL = (Environment.GetEnvironmentVariable("AppSettings__BaseUrl") ?? "http://localhost") + ":" + (Environment.GetEnvironmentVariable("DAPR_HTTP_PORT") ?? "3500");
-
-				//// Get secret from a local secret store
-				////var secret = await httpClient.GetStringAsync($"{baseURL}/v1.0/secrets/{DAPR_SECRET_STORE}/{SECRET_NAME}");
-				var secrectBulk = await httpClient.GetStringAsync($"{baseURL}/v1.0/secrets/{DAPR_SECRET_STORE}/bulk");
-				var secrectDict = JsonSerializer.Deserialize<Dictionary<string, Dictionary<string, string>>>(secrectBulk) ?? [];
-				Dictionary<string, string?> secrects = [];
-				foreach (var item in secrectDict)
-				{
-					if (item.Value == null || secrects.ContainsKey(item.Key)) continue;
-					secrects.Add(item.Key, item.Value[item.Key]);
-				}
-				IEnumerable<KeyValuePair<string, string?>> initialData = secrects;
-				jsonBuilder.AddInMemoryCollection(initialData);
-			}
-		}
-
-		jsonBuilder.AddEnvironmentVariables()
-			.Build()
-			.ExpandEnvironmentVariables(builder);
-
-		return builder;
-	}
-	static IConfigurationRoot? ExpandEnvironmentVariables(this IConfigurationRoot configurtion, dynamic builder)
-	{
-		if (configurtion == null) return null;
-		if (builder == null) return null;
-		IEnumerable<IConfigurationProvider> providers = configurtion.Providers.Where(t => t.GetType() == typeof(JsonConfigurationProvider)).AsEnumerable();
-		string? envVariablesConfig = builder.Configuration["ENV_VARIABLES"];
-		string[]? envVariables = envVariablesConfig?.Split(",");
-		foreach (var item in providers)
-		{
-			PropertyInfo? propInfo = typeof(JsonConfigurationProvider).GetProperty("Data", BindingFlags.NonPublic |
-																		BindingFlags.Instance | BindingFlags.GetField);
-			if (propInfo == null) continue;
-			object? settingVariables = propInfo.GetValue(item);
-			if (settingVariables == null) continue;
-			foreach ((string key, string value) in ((Dictionary<string, string>)settingVariables).Where(t => t.Key != null && t.Value != null))
-			{
-				if ((envVariables != null && !envVariables.Any(s => value.Contains(s)))
-					|| !value.Contains('%')) continue;
-				string settingValue = value;
-				settingValue = settingValue.ToExpandEnvironmentVariable();
-				builder.Configuration[key] = settingValue;
-			}
-		}
-
-		return configurtion;
 	}
 	static string ToExpandEnvironmentVariable(this string? variableName)
 	{
